d58d29a78a3c71b3fbc81d3681a4659033650e05
Andrei Harbuz <nerev94@gmail.com>
2020-03-23 02:06:59 +0300

    feat: solve the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (!Array.isArray(array) || array.length == 0) return 0;
+    let minItem = array[0];
+    array.forEach( item => minItem = item < minItem ? item : minItem);
+    return minItem;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (!Array.isArray(array) || array.length == 0) return 0;
+    let maxItem = array[0];
+    array.forEach( item => maxItem = item > maxItem ? item : maxItem);
+    return maxItem;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!Array.isArray(array) || array.length == 0) return 0;
+    return array.reduce( (sum, currentItem) => sum + currentItem ) / array.length;
 }
